0d8d91fd26333e8ff9d1d361d7b403dcaa81c1eb
ananthanarayanan16 <ananthanarayanan16h@gmail.com>
2025-05-13 12:23:51 +0530

    Goto_Task

---
+++ GOTO/goto.cpp
@@ -0,0 +1,38 @@
+// To print first ten natural numbers using goto statement.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n = 1;
+
+    print:
+
+    cout<< "\n" << n;
+    n = n + 1;
+
+    if(n <= 10)
+    {
+        goto print;
+    }
+
+    return 0;
+}
+
+/*
+
+output : 
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+*/
