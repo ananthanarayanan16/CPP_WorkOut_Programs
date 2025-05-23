b2b3da2e955cd7cb78ca9f947367f1b571ef2f17
ananthanarayanan16 <ananthanarayanan16h@gmail.com>
2025-05-23 20:32:52 +0530

    Print_10_Natural_Numbers

---
+++ LOOP/1-natural_numbers.cpp
@@ -0,0 +1,35 @@
+// Write a C++ program to display the first 10 natural numbers.
+
+#include"iostream"
+using namespace std;
+
+int main()
+{
+    int i;
+
+    cout<< "First 10 natural numbers are : \n";
+
+    for(i = 1; i <= 10; i++)
+    {
+        cout<< i << endl;
+    }
+
+    return 0;
+}
+
+/*
+output :
+
+First 10 natural numbers are : 
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+*/
