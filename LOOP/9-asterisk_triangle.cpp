4bcea03fe33f0f47d77c5e054afe0afb0c4f57de
ananthanarayanan16 <ananthanarayanan16h@gmail.com>
2025-05-29 21:09:33 +0530

    Dispale_Right_Angle_Triangle_Use_Asterisk_Symbol

---
+++ LOOP/9-asterisk_triangle.cpp
@@ -0,0 +1,32 @@
+// Write a C++ program to display a pattern like a right angle triangle using an asterisk.
+
+#include"iostream"
+using namespace std;
+
+int main()
+{
+    int i, j;
+
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            cout<< "* ";
+        }
+        cout<< endl;
+    }
+
+    return 0;
+}
+
+/*
+
+output :
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+
+*/
