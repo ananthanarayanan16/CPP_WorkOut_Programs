63e49e5fef3b6d1d03793458db1a5153aa7a4a9f
ananthanarayanan16 <ananthanarayanan16h@gmail.com>
2025-07-05 20:24:26 +0530

    Sum_All_Array_Elements

--- ARRAY/4-sum_array.cpp
+++ ARRAY/4-sum_array.cpp
@@ -12,14 +12,28 @@ int main()
 
     int arr[size];
 
-    cout<< "Input " << size << " elements in the array : ";
+    cout<< "Input " << size << " elements in the array : \n";
     for(int i = 0; i < size; i++)
     {
         cout<< "Element - " << i << " : ";
         cin>> arr[i];
         sum = sum + arr[i];
     }
-    cout<< "Sum of all elements stroed in the array is : " << sum;
+    cout<< "Sum of all elements stroed in the array is : " << sum << endl;
 
     return 0;
 }
+
+/*
+output :
+
+Input the number of elements to be stored in the array : 5
+Input 5 elements in the array : 
+Element - 0 : 1  
+Element - 1 : 2
+Element - 2 : 3
+Element - 3 : 4
+Element - 4 : 5
+Sum of all elements stroed in the array is : 15
+
+*/
