e42d5272719d8ae25890f09b94f282cfd74769b3
ananthanarayanan16 <ananthanarayanan16h@gmail.com>
2025-07-17 22:14:51 +0530

    Print_String

---
+++ STRING/1-print_string.cpp
@@ -0,0 +1,31 @@
+// Write a program in C++ to print string.
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string name;
+
+    cout<< "Enter your name : ";
+    cin>> name;
+    cout<< "Your name is : " << name << endl;
+
+    return 0;
+}
+
+
+/*
+
+output 1 :
+
+Enter your name : anantha narayanan
+Your name is : anantha
+
+output 2 :
+
+Enter your name : anantha_narayanan
+Your name is : anantha_narayanan
+
+*/
